6f24b2e79ca5ec40dabeabeebf21b7a9d1a9244e
RasmusFredensborg <rasmusfredensborg@gmail.com>
2016-02-10 16:49:02 +0100

    Tilføjet Developerkey klasse til at håndtere youtube api key.

---
+++ FasteApp-Test/app/src/main/java/com/example/rasmus/fasteapp_test/DeveloperKey.java
@@ -0,0 +1,8 @@
+package com.example.rasmus.fasteapp_test;
+
+/**
+ * Created by rasmus on 10-02-2016.
+ */
+public class DeveloperKey {
+    public static final String DEVELOPER_KEY = "283067764272-5jjjdp8hd6jm450h974midejngtu98ec.apps.googleusercontent.com";
+}
--- FasteApp-Test/app/src/main/java/com/example/rasmus/fasteapp_test/RasmusJActivity.java
+++ FasteApp-Test/app/src/main/java/com/example/rasmus/fasteapp_test/RasmusJActivity.java
@@ -3,11 +3,29 @@ package com.example.rasmus.fasteapp_test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RasmusJActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class RasmusJActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rasmus_j);
+
+        YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(DeveloperKey.DEVELOPER_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.cueVideo("wKJ9KzGQq0w");
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
     }
 }
