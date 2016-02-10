468fa745fd84707e21ddcfcc2f3beb5489110166
rbks <au@rbks.dk>
2016-02-10 17:06:48 +0100

    Rettelser i youtubefragment samt xml-layout

--- FasteApp-Test/app/src/main/java/com/example/rasmus/fasteapp_test/youtube.java
+++ FasteApp-Test/app/src/main/java/com/example/rasmus/fasteapp_test/youtube.java
@@ -2,8 +2,9 @@ package com.example.rasmus.fasteapp_test;
 
 import android.os.Bundle;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerFragment;
-import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 
 
 public class youtube extends YouTubeFailureRecoveryActivity {
@@ -11,9 +12,24 @@ public class youtube extends YouTubeFailureRecoveryActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.yt_fragment);
+
+        YouTubePlayerFragment youTubePlayerFragment = (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
+        youTubePlayerFragment.initialize(DeveloperKey.DEVELOPER_KEY, this);
     }
 
-    YouTubePlayerFragment youTubePlayerFragment = (YouTubePlayerFragment) getFragmentManager().findFragmentById()
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo("nCgQDjiotG0");
+        }
+    }
+
+    @Override
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
+    }
 
 
 }
