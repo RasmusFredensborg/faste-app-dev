6e763cedddb7a0079883979a7a3ae493feafc47d
RasmusFredensborg <rasmusfredensborg@gmail.com>
2016-02-10 15:36:19 +0100

    Tilføjet aktivitet så begge Rasmus'er kan arbejde :-)

---
+++ FasteApp-Test/app/src/main/java/com/example/rasmus/fasteapp_test/RasmusJActivity.java
@@ -0,0 +1,13 @@
+package com.example.rasmus.fasteapp_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RasmusJActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rasmus_j);
+    }
+}
