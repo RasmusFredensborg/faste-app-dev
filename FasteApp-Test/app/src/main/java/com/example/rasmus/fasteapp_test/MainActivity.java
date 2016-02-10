e95d4da694e9ba394bf14ad7f05e9d22194e9d6d
RasmusFredensborg <rasmusfredensborg@gmail.com>
2016-02-10 15:13:30 +0100

    Tilføjet youtube api

--- FasteApp-Test/app/src/main/java/com/example/rasmus/fasteapp_test/MainActivity.java
+++ FasteApp-Test/app/src/main/java/com/example/rasmus/fasteapp_test/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rasmus.fasteapp_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.youtube.player.YouTubePlayer;
 
 public class MainActivity extends AppCompatActivity {
 
