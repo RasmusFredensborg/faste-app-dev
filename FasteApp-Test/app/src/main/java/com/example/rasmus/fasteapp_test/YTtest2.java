da4e9f02b558f2d2fbd452a97dfbd1b2b292533c
rbks <au@rbks.dk>
2016-02-15 18:06:35 +0100

    Mandagens arbejde
    Main activity: Har kun rettet hvilken klasse den skal pege på
    
    Manifest: Har indsat ændringer der skulle til for at køre
    fuldskærmstilstand
    
    Youtube-klassen:  har prøvet forskellige ting med fragments men er gået
    bort fra denne
    
    YTtest2: Uden fragments, men kan gå i fullscreen og tilbage uden
    rebuffer. Er en "light"-version af fullscreen_demo - skal stadig ryddes
    op og skrives lidt om

---
+++ FasteApp-Test/app/src/main/java/com/example/rasmus/fasteapp_test/YTtest2.java
@@ -0,0 +1,98 @@
+package com.example.rasmus.fasteapp_test;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class YTtest2 extends YouTubeFailureRecoveryActivity implements YouTubePlayer.OnFullscreenListener {
+
+    final static int PORTRAIT_ORIENTATION = Build.VERSION.SDK_INT < 9
+            ? ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            : ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT;
+
+    private YouTubePlayerView playerView;
+    private YouTubePlayer player;
+    private View otherViews;
+
+    private boolean fullscreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yttest2);
+
+        playerView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        otherViews = findViewById(R.id.other_views);
+
+        playerView.initialize(DeveloperKey.DEVELOPER_KEY, this);
+
+        doLayout();
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+        this.player = player;
+        player.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_CUSTOM_LAYOUT);
+        player.setOnFullscreenListener(this);
+        int controlFlags = player.getFullscreenControlFlags();
+        setRequestedOrientation(PORTRAIT_ORIENTATION);
+        controlFlags |= YouTubePlayer.FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE;
+        player.setFullscreenControlFlags(controlFlags);
+        if (!wasRestored) {
+            player.cueVideo("avP5d16wEp0");
+        }
+    }
+
+    @Override
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return playerView;
+    }
+
+    private void doLayout() {
+        LinearLayout.LayoutParams playerParams = (LinearLayout.LayoutParams) playerView.getLayoutParams();
+        if(fullscreen) {
+            playerParams.width = LayoutParams.MATCH_PARENT;
+            playerParams.height = LayoutParams.MATCH_PARENT;
+
+            otherViews.setVisibility(View.GONE);
+        }
+        else {
+            otherViews.setVisibility(View.VISIBLE);
+            ViewGroup.LayoutParams otherViewsParams = otherViews.getLayoutParams();
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                playerParams.width = otherViewsParams.width = 0;
+                playerParams.height = LayoutParams.WRAP_CONTENT;
+                otherViewsParams.height = MATCH_PARENT;
+                playerParams.weight = 1;
+            } else {
+                playerParams.width = otherViewsParams.width = MATCH_PARENT;
+                playerParams.height = WRAP_CONTENT;
+                playerParams.weight = 0;
+                otherViewsParams.height = 0;
+            }
+        }
+    }
+
+    @Override
+    public void onFullscreen(boolean isFullScreen) {
+        fullscreen = isFullScreen;
+        doLayout();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration configuration) {
+        super.onConfigurationChanged(configuration);
+        doLayout();
+    }
+}
--- FasteApp-Test/app/src/main/java/com/example/rasmus/fasteapp_test/youtube.java
+++ FasteApp-Test/app/src/main/java/com/example/rasmus/fasteapp_test/youtube.java
@@ -1,6 +1,9 @@
 package com.example.rasmus.fasteapp_test;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -8,6 +11,8 @@ import com.google.android.youtube.player.YouTubePlayerFragment;
 
 
 public class youtube extends YouTubeFailureRecoveryActivity {
+
+    private YouTubePlayer player;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,8 @@ public class youtube extends YouTubeFailureRecoveryActivity {
 
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+        this.player = player;
+        player.setFullscreenControlFlags(YouTubePlayer.FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE);
         if (!wasRestored) {
             player.cueVideo("nCgQDjiotG0");
         }
@@ -29,6 +36,16 @@ public class youtube extends YouTubeFailureRecoveryActivity {
     @Override
     protected YouTubePlayer.Provider getYouTubePlayerProvider() {
         return (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
+    }
+
+    public void switchvid(View view) {
+        player.cueVideo("-Wnh9FYrcoE");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
     }
 
 
